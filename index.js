db988ab2760e149caf9a2da7bc81604dfb72cc5d
= <=>
2021-01-20 21:38:57 -0300

    size buttons

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 function sub() {
-    var result = document.querySelector("input");
+    let result = document.querySelector("input");
     result.value--;
 }
 function add() {
-    var result = document.querySelector("input");
+    let result = document.querySelector("input");
     result.value++;
 
 }
-function color() {
-    document.addEventListener('click', function (e) {
-        var id = (e.target.id);
-        var elemento = id;
-        document.getElementById(id).style.border = 'solid 2px #e67e34';
-        return elemento;
+
+let controles = [
+    {
+        idElement: "S",
+        isSelected: 1,
+    },
+    {
+        idElement: "M",
+        isSelected: 1,
+    },
+    {
+        idElement: "L",
+        isSelected: 1,
+    }
+]
+
+
+function color(objeto) {
+
+    let elemento = controles.find(e => e.idElement == objeto.id);
+
+    this.alterColor(elemento);
+
+    controles.forEach(e => {
+        if (e.idElement != objeto.id) {
+            e.isSelected = 0;
+            alterColor(e);
+            return;
+        }
+        e.isSelected = 1;
+        alterColor(e);
     });
+}
+
+
+function alterColor(elemento) {
+    if (elemento.isSelected == 0) {
+        gray(elemento);
+        return;
+    }
+    orange(elemento);
+}
+
+function orange(element) {
+    document.getElementById(element.idElement).style.border = 'solid 2px #e67e34';
+    element.isSelected = 1;
+}
+
+function gray(element) {
+    document.getElementById(element.idElement).style.border = 'solid 2px lightgray';
+    element.isSelected = 0;
+
 }
